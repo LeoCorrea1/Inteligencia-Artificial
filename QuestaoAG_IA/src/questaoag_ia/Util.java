685ad1b8325d3961f1bf3bc4de341e58a68c9ee6
Leonardo Vasconcelos Correa <leonardo.vasconcelos@ufn.edu.br>
2026-04-30 08:44:31 -0300

    Update Util.java

--- QuestaoAG_IA/src/questaoag_ia/Util.java
+++ QuestaoAG_IA/src/questaoag_ia/Util.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package questaoag_ia;
 
+import java.util.Random;
+
 /**
- *
- * @author laboratorio
+ * Classe que contém as letras do alfabeto no atributo letras. Além do método de gerarPalavra()
+ * @author alexandrezamberlan
  */
 public class Util {
+
+    /**
+     * Atributo de classe que armazena todos os números das cidades
+     */
+    public static String letras = "123456789";
+    /**
+     * Atributo de classe para guardar o tamanho do alfabeto
+     */
+    public static int tamanho = letras.length();
     
+    /**
+     * Método de classe que recebe a quantidade de caracteres que deve gerar a palavra
+     * @param n quantidade de letras que a palavra gerada terá
+     * @return uma palavra gerada aleatoriamente
+     */
+    public static StringBuffer gerarPalavra(int n) {
+        StringBuffer palavra = new StringBuffer();
+        Random gerador = new Random();
+        for (int i = 0; i < n; i++) {
+            palavra.append(letras.charAt(gerador.nextInt(tamanho)));
+        }
+        return palavra;
+    }
 }
